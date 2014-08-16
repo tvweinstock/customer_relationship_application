f709d93de20c84f224ce8801fa21b539645222b6
Tobi Weinstock <tv.weinstock@gmail.com>
2014-08-16 13:30:54 -0400

    Move methods into CRM class

--- cra.rb
+++ cra.rb
@@ -1,37 +1,44 @@
-# puts "\e[H\e[2j"
-
-def print_main_menu
-	puts "[1] Add a new contact"
-	puts "[2] Modify an existing contact"
-	puts "[3] Delete a contact"
-	puts "[4] Display all the contacts"
-	puts "[5] Display an attribute"
-	puts "[6] Exit"
-	puts "Enter a number:"
-	
-end
-
-
-def main_menu
-	print_main_menu
-	selection = gets.chomp.to_i
-	call_option(selection)
-end
-
-def call_option(selection)
-	case selection
-	when 1 then add_new_contact
-	when 2 then modify_contact
-	when 3 then delete_contact
-	when 4then display_contact
-	when 5 then display_attribute
-	when 6
-		puts "Goodbye!"
-		return
-	else
-		puts "Invalid option. Please try again."
-		main_menu
+class CRM
+	attr_accessor :name
+	def initialize(name)
+		@name = name
 	end
+
+	def print_main_menu
+		puts "[1] Add a new contact"
+		puts "[2] Modify an existing contact"
+		puts "[3] Delete a contact"
+		puts "[4] Display all the contacts"
+		puts "[5] Display an attribute"
+		puts "[6] Exit"
+		puts "Enter a number:"
+
+	end
+
+	def main_menu
+		puts "Welcome to #{name}"
+		print_main_menu
+		selection = gets.chomp.to_i
+		call_option(selection)
+	end
+
+	def call_option(selection)
+		case selection
+		when 1 then add_new_contact
+		when 2 then modify_contact
+		when 3 then delete_contact
+		when 4 then display_contact
+		when 5 then display_attribute
+		when 6
+			puts "Goodbye!"
+			return
+		else
+			puts "Invalid option. Please try again."
+			main_menu
+		end
+	end
+
 end
 
-main_menu
+crm = CRM.new("Bitmaker Labs CRM")
+crm.main_menu
